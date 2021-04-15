a2e2706d9e7d30ff39f28b01ec05e387c8f42a63
Sara Reed <reedsara90@gmail.com>
2021-04-15 15:17:21 -0600

    second afternoon commit

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -5,36 +5,38 @@ import Nav from './Nav/Nav';
 
 const Recipe = (props) => {
     const { id } = props.match.params
-    const [recipe, setRecipe] = useState([])
+    console.log(id)
+    const [recipe, setRecipe] = useState({})
 
     useEffect(() => {
         axios.get(`/api/recipe/${id}`)
         .then((res) => {
-            setRecipe(res.data)
             console.log(res.data)
+            setRecipe(res.data)
         })
         .catch(err => console.log(err))
     }, [id])
 console.log(recipe)
 
-    const recipeMap = recipe.params.map((recipe) => {
-        console.log(recipe.params)
-        return (
-            <div>
-            <img src={recipe.img} alt={recipe.title} className="recipe-images" />
-            <span className="recipe-details">
-            <p>{recipe.title}</p>
-            <p className="recipe-servings">{recipe.servings}</p>
-            <p>{recipe.timeframe}</p>
-            </span>
-            </div>
-        )
-    })
+    // const recipeMap = recipe.params.map((recipe) => {
+    //     console.log(recipe.params)
+    //     return (
+    //         <div>
+    //         <img src={recipe.img} alt={recipe.title} className="recipe-images" />
+    //         <span className="recipe-details">
+    //         <p>{recipe.title}</p>
+    //         <p className="recipe-servings">{recipe.servings}</p>
+    //         <p>{recipe.timeframe}</p>
+    //         </span>
+    //         </div>
+    //     )
+    // })
 
     return (
         <div>
             <Nav />
-            {recipeMap}
+            <h1>{recipe.title}</h1>
+            {/* {recipeMap} */}
         </div>
     )
 
