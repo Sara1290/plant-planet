328ea3c3060c2cc4f7e96a00671beac84951c65f
Sara Reed <reedsara90@gmail.com>
2021-04-20 14:40:11 -0600

    auth, dash rbox

--- src/Components/Dash.js
+++ src/Components/Dash.js
@@ -1,121 +1,154 @@
 //as soon as the user logs in they're taken to their dashboard
-//they should see the nav bar on the left. 
+//they should see the nav bar on the left.
 //they should see a search bar at the top.
 //immediatly on loading the user needs to see all the recipes ==> so useEffect to /api/recipes (get all recipes)
-import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom'
-import {useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 // import {getUser} from '../redux/userReducer'
-import Nav from './Nav/Nav';
-import './Dash.scss'
+import Nav from "./Nav/Nav";
+import "./Dash.scss";
 
 const Dash = (props) => {
-    const [dashRecipes, setDashRecipes] = useState([]);
-    const [search, setSearch] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-    const { username } = useSelector((state) => state.userReducer)
+  const [dashRecipes, setDashRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const { username } = useSelector((state) => state.userReducer);
 
-    const handleChange = e => {
-        setSearch(e.target.value);
-    }
-    //search 
-    useEffect(() => {
-        // axios.get("/api/recipes")
-        const results = dashRecipes.filter(recipe =>
-            recipe.title.includes(search)
-            )
-        setSearchResults(results);
-    }, [search]) //if i add dashRecipes the dap. array then ..the delete button goes away.. and it's dependent on a recipe author_username matching a logged in username... but in the console i'm seeing a user logged in.
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  //search
+  useEffect(() => {
+    // axios.get("/api/recipes")
+    const results = dashRecipes.filter((recipe) =>
+      recipe.title.includes(search)
+    );
+    setSearchResults(results);
+  }, [search]); //if i add dashRecipes the dap. array then ..the delete button goes away.. and it's dependent on a recipe author_username matching a logged in username... but in the console i'm seeing a user logged in.
 
-    //show all the recipes
-    useEffect(() => {
-        getRecipes()
+  //show all the recipes
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-    }, [])
+  const getRecipes = () => {
+    axios
+      .get("/api/recipes")
+      .then((res) => {
+        setDashRecipes(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const getRecipes = () => {
-        axios 
-            .get("/api/recipes")
-            .then((res) => {
-                setDashRecipes(res.data)
-            }).catch(err => console.log(err))
-    }
+  //save to the Recipe Box
+  const addToRecipeBox = (plant_recipes_id) => {
+    axios.post(`/api/save/${plant_recipes_id}`);
+  };
 
-    //save to the Recipe Box
-    const addToRecipeBox = (plant_recipes_id) => {
-        axios.post(`/api/save/${plant_recipes_id}`)
+  //logged in user can delete their submitted recipe
+  const deleteRecipe = (id) => {
+    axios.delete(`/api/delete/${id}`).then(() => {
+      getRecipes();
+    });
+  };
 
-    }
-   
-    //logged in user can delete their submitted recipe
-    const deleteRecipe = (id) => {
-        axios.delete(`/api/delete/${id}`)
-        .then(() => {
-            getRecipes()
-        })
-    }
-
-    //map over all the recipes
-    let mappedRecipes = dashRecipes.map((recipes) => {
-        return (
-            <div key={recipes.plant_recipes_id} className="recipe-container">
-            <Link to={`/recipe/${recipes.plant_recipes_id}`}>    
-            <img src={recipes.img} alt={recipes.title} className="recipe-images" />
-            </Link>
-            {
-                recipes.author_username === username
-                ? <button onClick={() => deleteRecipe(recipes.plant_recipes_id)}>Delete Your Recipe</button>
-                : null
-            }
-            <button className="add-button" onClick={() => addToRecipeBox(recipes.plant_recipes_id)} >{<img alt="" className="add-button-img" src="https://www.clipartkey.com/mpngs/m/50-505406_plus-sign-icon-button-green-approved-check-add.png" />}</button>
-            <span className="recipe-details">
-            <p>{recipes.title}</p>
-            {/* <p className="recipe-servings">{recipes.servings}</p> */}
-            <p>{recipes.timeframe}</p>
-            </span>
-            </div>
-            
-        )
-    })
-    console.log(dashRecipes)
-    
-
-    //map over the recipes that match search results
-    let mappedSearch = searchResults.map((recipes) => {
-        return (
-
-            <div className="recipe-container">
-            <Link to={`/recipe/${recipes.plant_recipes_id}`}>    
-            <img src={recipes.img} alt={recipes.title} className="recipe-images" />
-            </Link>
-            {}
-            <button className="add-button" onClick={() => addToRecipeBox(recipes.plant_recipes_id)} >{<img alt="" className="add-button-img" src="https://www.clipartkey.com/mpngs/m/50-505406_plus-sign-icon-button-green-approved-check-add.png" />}</button>
-            <span className="recipe-details">
-            <p>{recipes.title}</p>
-            {/* <p className="recipe-servings">{recipes.servings}</p> */}
-            <p>{recipes.timeframe}</p>
-            </span>
-            </div>
-        )
-    })
-
-    //the return for what's going to show up for the user.
+  //map over all the recipes
+  let mappedRecipes = dashRecipes.map((recipes) => {
     return (
-        <div className="dash-body">
-            <div className="welcome-search-container">
-            <h1>Welcome Back,<br/> {username}! </h1>
-            <input className="search-input" type="text" placeholder="Search By Title" value={search} onChange={handleChange} />
-            </div>
-            <Nav />
-            <div className="outer-recipe-container">
-            {searchResults.length < 1 
-            ? mappedRecipes 
-            : mappedSearch }
-            </div>
-            
-        </div>
-    )
-}
+      <div key={recipes.plant_recipes_id} className="recipe-container">
+        <Link to={`/recipe/${recipes.plant_recipes_id}`}>
+          <img
+            src={recipes.img}
+            alt={recipes.title}
+            className="recipe-images"
+          />
+        </Link>
+        {recipes.author_username === username ? (
+          <button onClick={() => deleteRecipe(recipes.plant_recipes_id)}>
+            Delete Your Recipe
+          </button>
+        ) : null}
+        <button
+          className="add-button"
+          onClick={() => addToRecipeBox(recipes.plant_recipes_id)}
+        >
+          {
+            <img
+              alt=""
+              className="add-button-img"
+              src="https://www.clipartkey.com/mpngs/m/50-505406_plus-sign-icon-button-green-approved-check-add.png"
+            />
+          }
+        </button>
+        <span className="recipe-details">
+          <p>{recipes.title}</p>
+          {/* <p className="recipe-servings">{recipes.servings}</p> */}
+          <p>{recipes.timeframe}</p>
+        </span>
+      </div>
+    );
+  });
+  console.log(dashRecipes);
+
+  //map over the recipes that match search results
+  let mappedSearch = searchResults.map((recipes) => {
+    return (
+      <div className="recipe-container">
+        <Link to={`/recipe/${recipes.plant_recipes_id}`}>
+          <img
+            src={recipes.img}
+            alt={recipes.title}
+            className="recipe-images"
+          />
+        </Link>
+        {}
+        <button
+          className="add-button"
+          onClick={() => addToRecipeBox(recipes.plant_recipes_id)}
+        >
+          {
+            <img
+              alt=""
+              className="add-button-img"
+              src="https://www.clipartkey.com/mpngs/m/50-505406_plus-sign-icon-button-green-approved-check-add.png"
+            />
+          }
+        </button>
+        <span className="recipe-details">
+          <p>{recipes.title}</p>
+          {/* <p className="recipe-servings">{recipes.servings}</p> */}
+          <p>{recipes.timeframe}</p>
+        </span>
+      </div>
+    );
+  });
+
+  //the return for what's going to show up for the user.
+  return (
+    <div className="dash-body">
+      <div className="welcome-search-container">
+        <h1>
+          Welcome Back,
+          <br /> {username}!{" "}
+        </h1>
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search By Title"
+          value={search}
+          onChange={handleChange}
+        />
+      </div>
+      <Nav />
+      <div className="outer-recipe-container">
+        {searchResults.length < 1 
+        ? mappedRecipes 
+        : mappedSearch}
+      </div>
+    </div>
+  );
+};
 export default Dash;
--- src/Components/RecipeBox.js
+++ src/Components/RecipeBox.js
@@ -1,49 +1,50 @@
-import React from 'react';
-import axios from 'axios';
-import { useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {savedRecipes} from '../redux/recipeReducer';
-import Nav from './Nav/Nav';
-
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { savedRecipes } from "../redux/recipeReducer";
+import Nav from "./Nav/Nav";
+import "./RecipeBox.scss";
 
 const RecipeBox = (props) => {
-    const {saved} = useSelector((state) => state.recipeReducer)
-    const user = useSelector((state) => state.userReducer)
-    const dispatch = useDispatch();
-    const { id } = user;
+  const { saved } = useSelector((state) => state.recipeReducer);
+  const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+  const { id } = user;
 
-    useEffect(() => {
-        if (id){
-            axios.get(`/api/saved/${id}`)
-            .then((res) => {
-                dispatch(savedRecipes(res.data))
-            })
-            .catch(err => console.log(err))
-        }
-    }, [dispatch, id])
-console.log(saved)
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`/api/saved/${id}`)
+        .then((res) => {
+          dispatch(savedRecipes(res.data));
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [dispatch, id]);
+  console.log(saved);
 
-    let mappedRecipes = saved.map((recipes) => {
-        return (
-            <div className="recipe-container">
-            <img src={recipes.img} alt={recipes.title} className="recipe-images" />
-            <span className="recipe-details">
-            <p>{recipes.title}</p>
-            {/* <p className="recipe-servings">{recipes.servings}</p> */}
-            <p>{recipes.timeframe}</p>
-            </span>
-            </div>
-        )
-    })
-
+  let mappedRecipes = saved.map((recipes) => {
     return (
-        <div className="recipe-box-container">
-        <Nav />
+      <div className="recipe-container">
+        <img src={recipes.img} alt={recipes.title} className="recipe-images" />
+        <span className="recipe-details">
+          <p>{recipes.title}</p>
+          <p>{recipes.timeframe}</p>
+        </span>
+      </div>
+    );
+  });
+
+  return (
+    <div className="recipe-box-body">
+      <div className="recipe-box-container">
         <h1>Your Recipe Box</h1>
-        {mappedRecipes}
-        </div>
-    )
+        <Nav />
+        <div className="outer-recipe-container">{mappedRecipes}</div>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default RecipeBox
+export default RecipeBox;
